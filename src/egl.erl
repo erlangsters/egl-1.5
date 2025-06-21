ffb55053e00d9fa5fdfb32a6d4a77d74f998f9f4
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2025-06-22 03:43:31 +0800

    wip - experiment new way of loading nif

--- src/egl.erl
+++ src/egl.erl
@@ -496,9 +496,20 @@ It's a reference to an EGL display connection.
 .
 
 init() ->
-    PrivDir = code:priv_dir(?MODULE),
-    NifPath = filename:join(PrivDir, "beam-egl"),
-    ok = erlang:load_nif(NifPath, 0).
+    LibName = "beam-egl",
+    SoName = case code:priv_dir(?MODULE) of
+        {error, bad_name} ->
+            case filelib:is_dir(filename:join(["..", priv])) of
+                true ->
+                    filename:join(["..", priv, LibName]);
+                _ ->
+                    filename:join([priv, LibName])
+            end;
+        Dir ->
+            filename:join(Dir, LibName)
+    end,
+    io:format("Loading EGL NIF from ~s~n", [SoName]),
+    erlang:load_nif(SoName, 0).
 
 -doc("""
 Return a list of EGL frame buffer configurations that match specified
